d9628f9ca7fe19b25fdcdaae39a06bfd54cc0fdb
Gavin Kestner <90367737+gavink01@users.noreply.github.com>
2023-11-30 16:59:53 -0600

    Added the Database and Music Model

--- Assignment5_GC/Models/Music.cs
+++ Assignment5_GC/Models/Music.cs
@@ -4,7 +4,17 @@ namespace Assignment5_GC.Models
 {
     public class Music
     {
+<<<<<<< Updated upstream
         public required int MusicId { get; set; }
+=======
+        public int id { get; set; }
+        public string title { get; set; }
+        public string purchase_type { get; set; }
+        public string performer {  get; set; }
+        public string genre { get; set; }
+        public int year { get; set; }
+        public decimal price { get; set; }
+>>>>>>> Stashed changes
 
         public required string Genre { get; set; }
 
