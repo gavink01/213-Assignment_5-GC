d6275c2f1a8bbb83f2c9905e625257cfc3c5931d
Gavin Kestner <90367737+gavink01@users.noreply.github.com>
2023-11-30 16:44:39 -0600

    Created The Music Model and Database

---
+++ Assignment5_GC/Models/Music.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignment5_GC.Models
+{
+    public class Music
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string purchase_type { get; set; }
+        public string performer {  get; set; }
+        public string genre { get; set; }
+        public int year { get; set; }
+        public decimal price { get; set; }
+
+
+    }
+}
