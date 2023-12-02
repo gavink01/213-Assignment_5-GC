4cce7d88e2edca791e9dec821e3645e96ed6dec9
Gavin Kestner <90367737+gavink01@users.noreply.github.com>
2023-12-02 17:23:28 -0600

    Shopping Cart

--- Assignment5_GC/obj/Debug/net8.0/Assignment5_GC.AssemblyInfo.cs
+++ Assignment5_GC/obj/Debug/net8.0/Assignment5_GC.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment5_GC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fbe3fc9347a3f3c394d28dff04be0bc75c58db8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5644d31ac6010517cb44881289cf3a3c36be18b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment5_GC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment5_GC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
