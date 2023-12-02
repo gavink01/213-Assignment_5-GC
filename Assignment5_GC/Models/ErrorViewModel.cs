5b6c12328b81122f05148404af9c0e23fc779d1d
ColinCampbell <149631324+CColin5@users.noreply.github.com>
2023-12-02 13:37:02 -0600

    Deleted project

--- Assignment5_GC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Assignment5_GC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
