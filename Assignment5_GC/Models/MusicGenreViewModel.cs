ddfbeb10641295a4a04d6e0fa06d3508d5996cd5
Gavin Kestner <90367737+gavink01@users.noreply.github.com>
2023-12-04 22:17:07 -0600

    Final Working

---
+++ Assignment5_GC/Models/MusicGenreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+namespace Assignment5_GC.Models
+{
+    public class MusicGenreViewModel
+    {
+        public List<Music>? Musics { get; set; }
+        public SelectList? Genres { get; set; }
+        public SelectList? Performers { get; set; }
+        public string? MusicGenre { get; set; }
+        public string? MusicPerformers { get; set; }
+    }
+}
